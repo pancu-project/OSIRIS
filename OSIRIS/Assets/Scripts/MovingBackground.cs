00d535f104c620f460f8d79e11f1f2ca3ff3a4e8
Cha Seoyeon <seoyeon0833@gmail.com>
2024-07-22 16:06:47 +0900

    스테이지 1 플랫폼 배치

---
+++ OSIRIS/Assets/Scripts/MovingBackground.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackground : MonoBehaviour
+{
+    Vector2 targetPos;
+    [SerializeField] float length = 30.23f; // 이미지 길이
+
+    private void Update()
+    {
+        targetPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        if (transform.position.x + length < targetPos.x)
+        {
+            transform.position 
+                = new Vector2(transform.position.x + length * 2, transform.position.y);
+        }
+
+        // if (시체 조각 미회수 or 구역 재로드)
+        //      배경 스크린 좌표를 카메라 좌표로 설정
+    }
+}
