f0b0327783d75d71461ae22ff010d78c493158a6
Cha Seoyeon <seoyeon0833@gmail.com>
2024-07-29 23:22:29 +0900

    장애물 이미지 변경 및 Title, Start, 플레이어 준비 씬 생성

---
+++ OSIRIS/Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExitButton : MonoBehaviour
+{
+    public void OnExitButton()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        //Application.Quit();
+    }
+}
---
+++ OSIRIS/Assets/Scripts/UI/PauseButton.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] Sprite[] pauseImage;
+
+    Image buttonImage;
+    bool isTimeFlow = true;
+
+    private void Start()
+    {
+        buttonImage = GetComponent<Image>();
+
+        if (buttonImage == null)
+        {
+            buttonImage.sprite = pauseImage[0];
+        }
+    }
+
+    public void PauseButtonClicked()
+    {
+        if (isTimeFlow)
+        {
+            Time.timeScale = 0f;
+            buttonImage.sprite = pauseImage[1];
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+
+        isTimeFlow = !isTimeFlow;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isTimeFlow)
+            {
+                Time.timeScale = 0f;
+                buttonImage.sprite = pauseImage[1];
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                buttonImage.sprite = pauseImage[0];
+            }
+
+            isTimeFlow = !isTimeFlow;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonImage.sprite = pauseImage[1];
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (isTimeFlow) buttonImage.sprite = pauseImage[0];
+    }
+}
